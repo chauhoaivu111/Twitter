8f128669e9ee7ae0d978ae51f0d317c0a9f3aa2d
chauhoaivu111 <chauhoaivu111@gmail.com>
2022-05-23 21:58:10 +0700

    menubar

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './sass/index.scss'
 
 import reportWebVitals from './reportWebVitals';
 import Layout from './pages/layout/Layout';
+import './assets/boxicons-2.0.7/css/boxicons.min.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
--- src/pages/layout/Layout.jsx
+++ src/pages/layout/Layout.jsx
@@ -1,11 +1,25 @@
+import React from "react";
 
-
-import React from 'react'
+import { BrowserRouter, Route } from "react-router-dom";
+import Menu from "../../compenents/menu/Menu";
+import Routers from "../../config/Routers";
+import './Layout.scss'
 
 const Layout = () => {
   return (
-    <div>Layout</div>
-  )
-}
+    <BrowserRouter>
+      <Route
+        render={(props) => (
+          <div className="layout">
+            <Menu {...props} />
+            <div className="layout__content">
+              <Routers />
+            </div>
+          </div>
+        )}
+      />
+    </BrowserRouter>
+  );
+};
 
-export default Layout
+export default Layout;
